730ad1600a4cc5eaf7c0166f948927103c88389a
Stephen Traskal <Straskal@gmail.com>
2019-02-17 14:48:47 -0500

    hopefully fixing build

--- test/mocks/asset/MockTextureCache.h
+++ test/mocks/asset/MockTextureCache.h
@@ -5,16 +5,11 @@
 #include "gmock/gmock.h"
 
 #include "graphics/Texture.h"
-#include "assetcache/AssetCache.h"
+#include "asset/AssetCache.h"
 
 class MockTextureCache : public milk::AssetCache<milk::Texture>
 {
 public:
-    explicit MockTextureCache(const std::string& rootDir)
-            : AssetCache(rootDir)
-    {
-    }
-
     MOCK_METHOD1(load, std::shared_ptr<milk::Texture>(const std::string&));
     MOCK_METHOD0(freeUnreferencedAssets, void());
 };
