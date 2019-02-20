9adb68bbbf2f6a5bfaac39c261e523828ceb7b72
Stephen Traskal <Straskal@gmail.com>
2019-02-20 18:57:22 -0500

    updating cmake and formatting scene

--- src/scene/Scene.cpp
+++ src/scene/Scene.cpp
@@ -12,10 +12,10 @@
 #include "window/Window.h"
 
 milk::Scene::Scene(const std::string& name, ActorLoader& actorLoader)
-        : name_(name),
-          actorLoader_(actorLoader),
-          tilemap_(nullptr),
-          ended_(false)
+	: name_(name),
+	actorLoader_(actorLoader),
+	tilemap_(nullptr),
+	ended_(false)
 {
 }
 
@@ -23,122 +23,122 @@ milk::Scene::~Scene() = default;
 
 milk::Actor* milk::Scene::spawnActor(const std::string& actorName, milk::Vector2 position)
 {
-    int id = idGenerator_.popId();
+	int id = idGenerator_.popId();
 
-    auto actor = std::make_unique<Actor>(*this, id, actorName, position);
+	auto actor = std::make_unique<Actor>(*this, id, actorName, position);
 
-    auto pActor = actor.get();
+	auto pActor = actor.get();
 
-    if (ended_)
-        return pActor;
+	if (ended_)
+		return pActor;
 
-    actorsToSpawn_.emplace_back(std::move(actor));
+	actorsToSpawn_.emplace_back(std::move(actor));
 
-    return pActor;
+	return pActor;
 }
 
 milk::Actor* milk::Scene::spawnActor(const std::string& actorName, Vector2 position, const std::string& templateName)
 {
-    auto pActor = spawnActor(actorName, position);
+	auto pActor = spawnActor(actorName, position);
 
-    actorLoader_.load(*pActor, templateName);
+	actorLoader_.load(*pActor, templateName);
 
-    return pActor;
+	return pActor;
 }
 
 bool milk::Scene::destroyActor(int id)
 {
-    if (actorsById_.find(id) == actorsById_.end()
-        && std::find_if(actorsToSpawn_.begin(),
-                        actorsToSpawn_.end(),
-                        [&](const std::unique_ptr<Actor>& actor) -> bool { return actor->id() == id; }) == actorsToSpawn_.end())
-        return false;
+	if (actorsById_.find(id) == actorsById_.end()
+		&& std::find_if(
+			actorsToSpawn_.begin(),
+			actorsToSpawn_.end(),
+			[&](const std::unique_ptr<Actor>& actor) -> bool { return actor->id() == id; }) == actorsToSpawn_.end())
+		return false;
 
-    actorsToDestroy_.emplace_back(id);
+	actorsToDestroy_.emplace_back(id);
 
-    return true;
+	return true;
 }
 
 milk::Actor* milk::Scene::findActor(const std::string& name) const
 {
-    // TODO: Find a better solution than brute force implementation.
-    for (auto& itr : actorsToSpawn_)
-    {
-        if (itr->name() == name)
-            return itr.get();
-    }
+	for (auto& itr : actorsToSpawn_)
+	{
+		if (itr->name() == name)
+			return itr.get();
+	}
 
-    for (auto& it : actorsById_)
-    {
-        if (it.second->name() == name)
-            return it.second.get();
-    }
+	for (auto& it : actorsById_)
+	{
+		if (it.second->name() == name)
+			return it.second.get();
+	}
 
-    return nullptr;
+	return nullptr;
 }
 
 milk::Camera& milk::Scene::camera()
 {
-    return camera_;
+	return camera_;
 }
 
 milk::Tilemap* milk::Scene::tilemap()
 {
-    return tilemap_.get();
+	return tilemap_.get();
 }
 
 void milk::Scene::tilemap(std::unique_ptr<milk::Tilemap> tilemap)
 {
-    tilemap_ = std::move(tilemap);
+	tilemap_ = std::move(tilemap);
 }
 
 milk::Rectangle milk::Scene::bounds() const
 {
-    return Rectangle{0, 0, tilemap_->width, tilemap_->height};
+	return Rectangle{ 0, 0, tilemap_->width, tilemap_->height };
 }
 
 void milk::Scene::end()
 {
-    ended_ = true;
+	ended_ = true;
 }
 
 milk::Actor* milk::Scene::pollSpawned()
 {
-    if (actorsToSpawn_.empty())
-        return nullptr;
+	if (actorsToSpawn_.empty())
+		return nullptr;
 
-    auto& spawned = actorsToSpawn_.back();
+	auto& spawned = actorsToSpawn_.back();
 
-    auto pSpawned = spawned.get();
+	auto pSpawned = spawned.get();
 
-    actorsById_.insert(std::make_pair(spawned->id(), std::move(spawned)));
+	actorsById_.insert(std::make_pair(spawned->id(), std::move(spawned)));
 
-    actorsToSpawn_.pop_back();
+	actorsToSpawn_.pop_back();
 
-    return pSpawned;
+	return pSpawned;
 }
 
 milk::Actor* milk::Scene::pollDestroyed()
 {
-    static int lastPolledId = -1;
+	static int lastPolledId = -1;
 
-    if (actorsToDestroy_.empty())
-    {
-        lastPolledId = -1;
-        return nullptr;
-    }
+	if (actorsToDestroy_.empty())
+	{
+		lastPolledId = -1;
+		return nullptr;
+	}
 
-    if (lastPolledId > -1)
-    {
-        actorsById_.erase(lastPolledId);
-        idGenerator_.pushId(lastPolledId);
-    }
+	if (lastPolledId > -1)
+	{
+		actorsById_.erase(lastPolledId);
+		idGenerator_.pushId(lastPolledId);
+	}
 
-    int destroyedId = actorsToDestroy_.back();
+	int destroyedId = actorsToDestroy_.back();
 
-    lastPolledId = destroyedId;
+	lastPolledId = destroyedId;
 
-    actorsToDestroy_.pop_back();
+	actorsToDestroy_.pop_back();
 
-    return actorsById_.at(destroyedId).get();
+	return actorsById_.at(destroyedId).get();
 }
