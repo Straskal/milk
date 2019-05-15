832a495fc728cf13675bf53c9f93ba4bbed77792
Stephen Traskal <Straskal@gmail.com>
2019-05-15 16:54:36 -0400

    removing a TON of stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,6 @@
-#include "game/Game.h"
-
 #include "SDL_main.h"
 
 int main(int argc, char* argv[])
 {
-	auto& game = milk::Game::instance();
-
-	game.init("res/config.lua");
-
-	return game.run();
+	return 0;
 }
