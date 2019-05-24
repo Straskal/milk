5cce18652a036f3f63b7f6d253adefd28626d99e
Stephen Traskal <Straskal@gmail.com>
2019-05-23 22:40:56 -0400

    trying a more data oriented approach

--- src/data/Ids.h
+++ src/data/Ids.h
@@ -1,7 +1,6 @@
 #ifndef MILK_IDS_H
 #define MILK_IDS_H
 
-#include <assert.h>
 #include <string>
 #include <queue>
 #include <vector>
@@ -9,26 +8,18 @@
 #include "data/int.h"
 
 namespace milk {
-	class Ids {
-	public:
+	struct IdData {
+		std::queue<U16> freeIndeces;
+		std::vector<U16> generations;
+	};
+
+	namespace id {
 		const static U32 INVALID;
 
-		const U8 IDX_BITS;
-		const U8 GEN_BITS;
-		const U16 MAX;
-		const U16 MAX_FREE;
-
-		Ids();
-		Ids(U8 idxbits, U8 genbits, U16 max, U16 maxFree);
-
-		U32 create();
-		void remove(U32 id);
-		bool valid(U32 id) const;
-
-	private:
-		std::queue<U16> freeIndeces_{};
-		std::vector<U16> generations_{};
-	};
+		U32 create(IdData& ids);
+		void remove(IdData& ids, U32 id);
+		bool valid(IdData& ids, U32 id);
+	}
 }
 
 #endif
