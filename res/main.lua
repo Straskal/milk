069edbc31abaae7bb07f22ccb6687ac5e1222f69
Stephen Traskal <Straskal@gmail.com>
2019-06-13 22:57:07 -0400

    trying out deployment

--- res/main.lua
+++ res/main.lua
@@ -1,71 +1,34 @@
-local video = require "milk.video"
-local input = require "milk.input"
-
--- submodules
-local window = video.window
-local renderer = video.renderer
-local texture = video.texture
-local keyboard = input.keyboard
-local keys = keyboard.keys
+local video = require("milk.video")
 
 -- initialize game
-window.set_title("Butt Dragons")
-window.set_size(1280, 720)
-renderer.set_resolution(640, 360)
+video.window.set_title("Butt Dragons")
+video.window.set_size(1280, 720)
+video.renderer.set_resolution(640, 360)
 
-local tex = texture.new("res/player.png")
-local positions = {}
-local speed = 2
-for i = 1, 100 do
-	positions[i] = { x = i * 20, y = i * 20 }
-end
+local playertexture = video.texture.new("res/player.png")
+local playerposition = { x = 0, y = 0 }
+local sourcerectangle = { x = 0, y = 0, w = 64, h = 64 }
+local lastanimtime = 0
+local SECONDS_PER_ANIM_FRAME = 0.2
 
 -- callback table
 local callbacks = {};
 
-function callbacks.tick(dt)
-	if keyboard.was_key_released(keys.F) then 
-		local toggle = not window.is_fullscreen()
-		window.set_fullscreen(toggle) 
-	end
-
-	if keyboard.was_key_released(keys.ESCAPE) then 
-		window.close()
-	end
-
-	if keyboard.was_key_pressed(keys.L) then
-		local w, h = window.get_size()
-		print("window size:" .. " " .. w .. ", " .. h)
-	end
-
-	if keyboard.is_key_pressed(keys.W) then 
-		for i = 1, #positions do
-			positions[i].y = positions[i].y - speed
+-- game logic goes here
+function callbacks.tick()
+	local totaltime = os.clock()
+	if totaltime - lastanimtime > SECONDS_PER_ANIM_FRAME then
+		sourcerectangle.x = sourcerectangle.x + 64
+		if (sourcerectangle.x > 512) then
+			sourcerectangle.x = 0
 		end
-	end
-	if keyboard.is_key_pressed(keys.A) then 
-		for i = 1, #positions do
-			positions[i].x = positions[i].x - speed
-		end
-	end
-
-	if keyboard.is_key_pressed(keys.S) then 
-		for i = 1, #positions do
-			positions[i].y = positions[i].y + speed
-		end
-	end
-
-	if keyboard.is_key_pressed(keys.D) then 
-		for i = 1, #positions do
-			positions[i].x = positions[i].x + speed
-		end
+		lastanimtime = totaltime
 	end
 end
 
+-- render calls go here
 function callbacks.render()
-	for i = 1, #positions do		
-		renderer.drawex(tex, positions[i], { x = 0, y = 0, w = 64, h = 64 })
-	end
+	video.renderer.drawex(playertexture, playerposition, sourcerectangle)
 end
 
 return callbacks
