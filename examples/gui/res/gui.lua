4dab66b20cebb1c4e59125c0009c22011b0faf57
Stephen Traskal <Straskal@gmail.com>
2019-06-23 18:50:28 -0400

    updating example gui proj

--- examples/gui/res/gui.lua
+++ examples/gui/res/gui.lua
@@ -2,62 +2,93 @@ local mouse = require("milk.mouse")
 local graphics = require("milk.graphics")
 local mouse_buttons = mouse.buttons
 
+local layers = 0
+
+local style = {
+    button = {
+        default_color = { 1, 1, 1, 1 },
+        hot_color = { 0.1, 1, 1, 1 },
+        active_color = { 0.8, 1, 1, 1 },
+        disabled_color = { 0.3, 0.3, 0.3, 1 }
+    }
+}
+
 -- helpers
 local is_mouse_hover = function(mx, my, x, y, w, h)
     return mx > x and mx <= x + w and my > y and my <= y + h
 end
 
-local new = function()
-    local gui = {
-        -- state
-        mousex = 0,
-        mousey = 0,
-        is_down = false,
-        hot_id = -1,
-        active_id = -1,
-        -- styling
-        hot_color = { 0.1, 1, 1, 1 },
-        active_color = { 0.8, 1, 1, 1 }
-    }
+local push_id = function()
+    layers = layers + 1
+    return layers
+end
+
+local pop_id = function()
+    layers = layers - 1
+end
+
+local enabled = function(layer_id)
+    return layer_id == layers
+end
+
+local new_layer = function()
+    local layer_id = push_id()
+    local mousex = 0
+    local mousey = 0
+    local is_down = false
+    local hot_id = -1
+    local active_id = -1
+
+    local layer = {}
     
-    function gui.start(self)
-        self.hot_id = 0
-        self.mousex, self.mousey = mouse.get_position()
-        self.is_down = mouse.is_button_down(mouse_buttons.LEFT)
+    function layer:begin_frame()
+        hot_id = 0
+        mousex, mousey = mouse.get_position()
+        is_down = mouse.is_button_down(mouse_buttons.LEFT)
     end
     
-    function gui.stop(self)
-        if not self.is_down then 
-            self.active_id = 0
-        elseif self.active_id == 0 then
-            self.active_id = -1        
+    function layer:end_frame()
+        if not is_down then 
+            active_id = 0
+        elseif active_id == 0 then
+            active_id = -1        
         end
-    end 
+    end
+
+    function layer:remove()
+        layers = layers - 1
+    end
     
-    function gui.button(self, id, x, y, texture, src_rect)
-        if is_mouse_hover(self.mousex, self.mousey, x, y, src_rect.w, src_rect.h) then
-            self.hot_id = id
-            if self.active_id == 0 and self.is_down then
-                self.active_id = id
+    function layer:button(id, x, y, texture, src_rect)
+        if not enabled(layer_id) then
+            graphics.set_draw_color(table.unpack(style.button.disabled_color))
+            graphics.drawx(texture, x, y, src_rect.x, src_rect.y, src_rect.w, src_rect.h, 1, 0)
+            return false
+        end
+        if is_mouse_hover(mousex, mousey, x, y, src_rect.w, src_rect.h) then
+            hot_id = id
+            if active_id == 0 and is_down then
+                active_id = id
             end
         end
-        if self.hot_id == id then
-            if self.active_id == id then
-                graphics.set_draw_color(table.unpack(self.active_color))
+        if hot_id == id then
+            if active_id == id then
+                graphics.set_draw_color(table.unpack(style.button.active_color))
             else
-                graphics.set_draw_color(table.unpack(self.hot_color))
+                graphics.set_draw_color(table.unpack(style.button.hot_color))
             end
         else
-            graphics.set_draw_color(1, 1, 1, 1)
+            graphics.set_draw_color(table.unpack(style.button.default_color))
         end
         graphics.drawx(texture, x, y, src_rect.x, src_rect.y, src_rect.w, src_rect.h, 1, 0)
         -- if button is not down but control is hot and active then the user has clicked this button
-        return not self.is_down and self.hot_id == id and self.active_id == id
+        return not is_down and hot_id == id and active_id == id
     end
     
-    return gui
+    return layer
 end
 
 return {
-    new = new
+    new_layer = new_layer,
+    style = style
 }
--- examples/gui/res/state_menu.lua
+++ examples/gui/res/state_menu.lua
@@ -1,19 +1,18 @@
 local window = require("milk.window")
 local graphics = require("milk.graphics")
-local Gui = require("res.gui")
+local gui = require("res.gui")
 local settings_state = require("res.state_settings")
 
 -- buttons
-local RESUME = { x = 0, y = 0, w = 64, h = 64 }
-local SETTINGS = { x = 256, y = 0, w = 64, h = 64 }
-local QUIT = { x = 192, y = 0, w = 64, h = 64 }
+local SETTINGS = { x = 0, y = 0, w = 64, h = 32 }
+local QUIT = { x = 64, y = 0, w = 64, h = 32 }
 
 local state = {
-    menu_sheet = graphics.new_texture("res/menusheet.png"),
-    gui = Gui.new()
+    menu_sheet = graphics.new_texture("res/menusheet.png")
 }
 
 function state:enter(game)
+    self.gui_layer = gui.new_layer()
 end
 
 function state:exit(game)
@@ -23,21 +22,18 @@ function state:tick(game)
 end
 
 function state:draw(game)
-    -- draw black  overlay
+    -- draw black overlay
     graphics.set_draw_color(0, 0, 0, 0.7)
     graphics.draw_filled_rect(0, 0, 640, 360)
-    
-    self.gui:start()
-    if self.gui:button(1, 300, 50, self.menu_sheet, RESUME) then
-        game.pop_state()
-    end
-    if self.gui:button(2, 300, 120, self.menu_sheet, SETTINGS) then
+
+    self.gui_layer:begin_frame()
+    if self.gui_layer:button(1, 300, 120, self.menu_sheet, SETTINGS) then
         game.push_state(settings_state)
     end      
-    if self.gui:button(3, 300, 190, self.menu_sheet, QUIT) then
+    if self.gui_layer:button(2, 300, 190, self.menu_sheet, QUIT) then
         window.close()
     end
-    self.gui:stop()
+    self.gui_layer:end_frame()
 end
 
 return state
