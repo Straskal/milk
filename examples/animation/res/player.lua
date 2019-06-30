c68a439d1552b6899b81a238d41b04e6be7fdcc3
Stephen Traskal <Straskal@gmail.com>
2019-06-29 21:50:26 -0400

    animator

--- examples/animation/res/player.lua
+++ examples/animation/res/player.lua
@@ -1,6 +1,6 @@
 local graphics = require("milk.graphics")
 local keyboard = require("milk.keyboard")
-local animatedsprite = require("res.animatedsprite")
+local animator = require("res.animator")
 local flip_flags = graphics.flip_flags
 local keys = keyboard.keys
 
@@ -16,23 +16,23 @@ local Player_mt = {}
 function Player_mt:init()
     self.x = 0
     self.y = 0
+    self.image = graphics.new_image("res/player.png")
+    self.flip = flip_flags.NONE
     self.animations = {
         idle = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 },
         walk = { 15, 16, 17, 18, 19, 20, 21 }
     }
-    self.sprite = animatedsprite.new()
-    self.sprite:init({
-        texture_name = "res/player.png",
-        initial_anim = self.animations.idle,
+    self.animator = animator.new({
+        frame_height = 64,
+        frame_width = 64,
         rows = 4,
         columns = 8,
-        seconds_per_frame = 0.1
+        seconds_per_frame = 0.1,
+        initial_anim = self.animations.idle
     })
 end
 
 function Player_mt:tick()
-    self.sprite:tick()
-
     self.vx = 0
     self.vy = 0
 
@@ -44,26 +44,28 @@ function Player_mt:tick()
     end
     if keyboard.is_key_down(keys.A) then 
         self.vx = -1 * PLAYER_SPEED
-        self.sprite:flip_x(true)
+        self.flip = self.flip | flip_flags.X
     end
     if keyboard.is_key_down(keys.D) then 
         self.vx = 1 * PLAYER_SPEED
-        self.sprite:flip_x(false)
+        self.flip = self.flip & ~flip_flags.X
     end
 
     self.x = self.x + self.vx
     self.y = self.y + self.vy
     local anim = (((self.vx == 0 and self.vy == 0) and self.animations.idle) or self.animations.walk)
-    self.sprite:set_animation(anim)
+    self.animator:set_animation(anim)
 end
 
 function Player_mt:draw()
-    self.sprite:draw(self.x, self.y)
+    local sx, sy, sw, sh = self.animator:tick()
+    graphics.drawx(self.image, self.x, self.y, sx, sy, sw, sh, 1, self.flip)
 end
 
 function player.new()
     local self = {}
     setmetatable(self, { __index = Player_mt })
+    self:init()
     return self
 end
 
