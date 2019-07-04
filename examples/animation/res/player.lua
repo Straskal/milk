576d3948415e2c370545c82c323efd06d04796f9
Stephen Traskal <Straskal@gmail.com>
2019-07-03 20:36:45 -0400

    Update player.lua

--- examples/animation/res/player.lua
+++ examples/animation/res/player.lua
@@ -1,7 +1,7 @@
 local graphics = require("milk.graphics")
 local keyboard = require("milk.keyboard")
+local time = require("milk.time")
 local animator = require("res.animator")
-local flip_flags = graphics.flip_flags
 local keys = keyboard.keys
 
 -- constants
@@ -17,7 +17,7 @@ function Player_mt:init()
     self.x = 0
     self.y = 0
     self.image = graphics.new_image("res/player.png")
-    self.flip = flip_flags.NONE
+    self.flippedx = 1
     self.animations = {
         idle = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 },
         walk = { 15, 16, 17, 18, 19, 20, 21 }
@@ -33,6 +33,8 @@ function Player_mt:init()
 end
 
 function Player_mt:tick()
+	local dt = time.get_delta()
+
     self.vx = 0
     self.vy = 0
 
@@ -44,11 +46,11 @@ function Player_mt:tick()
     end
     if keyboard.is_key_down(keys.A) then 
         self.vx = -1 * PLAYER_SPEED
-        self.flip = self.flip | flip_flags.X
+        self.flippedx = -1
     end
     if keyboard.is_key_down(keys.D) then 
         self.vx = 1 * PLAYER_SPEED
-        self.flip = self.flip & ~flip_flags.X
+        self.flippedx = 1
     end
 
     self.x = self.x + self.vx
@@ -59,7 +61,7 @@ end
 
 function Player_mt:draw()
     local sx, sy, sw, sh = self.animator:tick()
-    graphics.drawx(self.image, self.x, self.y, sx, sy, sw, sh, 1, self.flip)
+    graphics.drawx(self.image, self.x - 32, self.y - 32, sx, sy, sw, sh, self.scale * self.flippedx, self.scale, self.angle)
 end
 
 function player.new()
