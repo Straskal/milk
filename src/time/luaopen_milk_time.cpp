fd64961b3e538c51148ba38e84efb0d08ff99dcf
Stephen Traskal <Straskal@gmail.com>
2019-07-07 22:24:01 -0400

    Update luaopen_milk_time.cpp

--- src/time/luaopen_milk_time.cpp
+++ src/time/luaopen_milk_time.cpp
@@ -7,28 +7,28 @@ extern "C" {
 
 #include "time.h"
 
-static int time_get_total(lua_State* L)
+static int lua_time_get_total(lua_State* L)
 {
 	double total = milk::time_get_total();
 	lua_pushnumber(L, total);
 	return 1;
 }
 
-static int time_delay(lua_State* L)
+static int lua_time_delay(lua_State* L)
 {
 	double seconds = (double)luaL_checknumber(L, 1);
 	milk::time_delay(seconds);
 	return 0;
 }
 
-static const luaL_Reg time_funcs[] = {
-	{ "get_total", time_get_total },
-	{ "delay", time_delay },
+static const luaL_Reg lua_time_funcs[] = {
+	{ "get_total", lua_time_get_total },
+	{ "delay", lua_time_delay },
 	{ nullptr, nullptr }
 };
 
 int luaopen_milk_time(lua_State* L)
 {
-	luaL_newlib(L, time_funcs);
+	luaL_newlib(L, lua_time_funcs);
 	return 1;
 }
