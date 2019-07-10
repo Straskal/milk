7f6d9d7baba0ff6db04373ee9684cfc0bf9d1dca
Stephen Traskal <Straskal@gmail.com>
2019-07-10 19:28:18 -0400

    Update mousetest.lua

--- tests/mousetest.lua
+++ tests/mousetest.lua
@@ -1,6 +1,8 @@
 local window = require("milk.window")
 local mouse = require("milk.mouse")
+local keyboard = require("milk.keyboard")
 local buttons = mouse.buttons
+local keys = keyboard.keys
 
 --[[
     the draw test proves the following:
@@ -13,10 +15,35 @@ function test:start()
 end
 
 function test:tick()
-    if mouse.is_button_released(buttons.LEFT) then
-        print("LEFT button released")
+    -- down
+    if keyboard.is_key_down(keys.LSHIFT) then
+        if mouse.is_button_down(buttons.LEFT) then
+            print("LEFT button down")
+        end
+        if mouse.is_button_down(buttons.MIDDLE) then
+            print("MIDDLE button down")
+        end
+        if mouse.is_button_down(buttons.RIGHT) then
+            print("RIGHT button down")
+        end
+    end
+
+    -- press
+    if mouse.is_button_pressed(buttons.LEFT) then
+        print("LEFT button pressed")
         local x, y = mouse.get_position()
         print("X: " .. x .. " Y: " .. y)
+    end
+    if mouse.is_button_pressed(buttons.MIDDLE) then
+        print("MIDDLE button pressed")
+    end
+    if mouse.is_button_pressed(buttons.RIGHT) then
+        print("RIGHT button pressed")
+    end
+
+    -- release
+    if mouse.is_button_released(buttons.LEFT) then
+        print("LEFT button released")
     end
     if mouse.is_button_released(buttons.MIDDLE) then
         print("MIDDLE button released")
