b9bf3d29a8f83b8e839ac0217d7a130146f87fe8
Stephen Traskal <Straskal@gmail.com>
2019-07-09 20:47:58 -0400

    updated tests

--- tests/testrunner.lua
+++ tests/testrunner.lua
@@ -1,15 +1,70 @@
+local milk = require("milk")
+local window = require("milk.window")
+local time = require("milk.time")
+local graphics = require("milk.graphics")
+
+--------------------------------------------
 local tests = {
     WINDOW_TEST = "tests/windowtest.lua",
-    DRAW_TEST = "tests/drawtest.lua"
+    DRAW_TEST = "tests/graphicstest.lua"
 }
+--------------------------------------------
+--------------------------------------------
+local function run(test)
+    local start = test.start or function() end
+    local tick = test.tick or function() end
+    local draw = test.draw or function() end
+    local stop = test.stop or function() end
+
+    milk.init()
+
+    -- start test
+    start(test)
+
+    window.show()
+    
+    local TARGET_FPS = 60
+    local SECONDS_PER_TICK = 1 / TARGET_FPS
+    local frame_start_time = 0
+    local frame_time = 0
+    local accumulated_frame_time = 0
+    
+    while not window.should_close() do
+        local t = time.get_total()
+        frame_time = t - frame_start_time
+        frame_start_time = t
+        accumulated_frame_time = accumulated_frame_time + frame_time
+    
+        while accumulated_frame_time >= SECONDS_PER_TICK do
+            window.poll()
+    
+            -- game logic
+            tick(test, SECONDS_PER_TICK)
+    
+            graphics.set_draw_color(0, 0, 0, 1)
+            graphics.clear()
+            graphics.set_draw_color(1, 1, 1, 1)
+            
+            draw(test, SECONDS_PER_TICK)
+            
+            graphics.present()
+            accumulated_frame_time = accumulated_frame_time - SECONDS_PER_TICK
+        end
+    end
+
+    stop(test)
+    
+    milk.quit()
+end
 
 local function run_test(name, file)
     print("=================================")
     print("RUNNING " .. name)
     print("=================================")
-    dofile(file)
+    run(dofile(file))
 end
-
+--------------------------------------------
+--------------------------------------------
 local test_to_run
 repeat
     print("TEST RUNNER")
--- tests/windowtest.lua
+++ tests/windowtest.lua
@@ -1,29 +1,73 @@
-local milk = require("milk")
 local window = require("milk.window")
 local time = require("milk.time")
 
+local stages = {
+    {
+        start = function()
+            print("setting size to 1280x720")
+            window.set_size(1280, 720)
+        end
+    },
+    {
+        start = function()
+            print("setting title to 'new title!'")
+            window.set_title("new title!")
+        end
+    },
+    {
+        start = function()
+            print("setting icon")
+            window.set_icon("tests/assets/milkicon.png")
+        end
+    },
+    {
+        start = function()
+            print("setting fullscreen to true")
+            window.set_fullscreen(true)
+        end
+    },
+    {
+        start = function()
+            print("setting fullscreen to false")
+            window.set_fullscreen(false)
+        end
+    }
+}
+
 --[[
-    the window test proves the following:
-    - the window fullscreen toggle works
-	- window sizing works
+    the draw test proves the following:
+    - the window's size settings work
+    - the window can be minimized
+    - the window can be restored
+    - the window's fullscreen setting can be toggled
 --]]
-milk.init()
+local test = {}
 
-window.show()
-print("window shown")
-print("window will enter fullscreen in 5 seconds...")
-time.delay(5)
-window.set_fullscreen(true)
-print("window is now in fullscreen mode")
-print("window will exit fullscreen in 5 seconds...")
-time.delay(5)
-window.set_fullscreen(false)
-print("window is now in windowed mode")
-print("window will change from 800x600 to 300x300 in 3 seconds...")
-time.delay(3)
-window.set_size(300, 300)
-print("window is now 300x300")
-print("test finished and will end in 3 seconds...")
-time.delay(3)
+function test:start()
+    self.stage_num = 1
+    self.stage = stages[self.stage_num]
+    self.stage.start()
+    self.stage.start_time = time.get_total() + 3
+end
 
-milk.quit()
+function test:tick()
+    local t = time.get_total()
+    if t > self.stage.start_time then
+        self.stage_num = self.stage_num + 1
+        if self.stage_num > #stages then
+            window.close()
+        else
+            self.stage = stages[self.stage_num]
+            self.stage.start()
+            self.stage.start_time = time.get_total() + 3
+        end
+    end
+end
+
+function test:draw()
+end
+
+function test:stop()
+end
+
+return test
