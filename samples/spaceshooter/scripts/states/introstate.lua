bd1828fec57e8d3eba2995e5bbe69f69f248b09e
Stephen Traskal <Straskal@gmail.com>
2020-06-06 17:07:01 -0400

    cool

--- samples/spaceshooter/scripts/states/introstate.lua
+++ samples/spaceshooter/scripts/states/introstate.lua
@@ -7,7 +7,7 @@ function introState.enter()
 end
 
 function introState.update()
-    if milk.btp(milk.btna) then milk.play(0, 0, 128) end
+    if milk.btnp(milk.btna) then milk.play(0, 0, 128) end
     if milk.btnp(milk.btnb) then milk.pause(0) end
     if milk.btnp(milk.btnx) then milk.resume(0) end
 end
