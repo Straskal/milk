a3c45ab0c241565538802a15a5cd59b2a8a4c4c2
Stephen Traskal <Straskal@gmail.com>
2020-06-12 16:34:20 -0400

    updatinv vergiajeee

--- src/cmd.h
+++ src/cmd.h
@@ -4,18 +4,18 @@
  *  Copyright(c) 2018 - 2020 Stephen Traskal
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software andassociated documentation files(the "Software"), to deal
+ *  of this software and associated documentation files(the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, andto permit persons to whom the Software is
+ *  copies of the Software, and to permit persons to whom the Software is
  *  furnished to do so, subject to the following conditions :
  *
- *  The above copyright notice andthis permission notice shall be included in all
+ *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
  *
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
  *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
@@ -43,10 +43,10 @@
 
 typedef enum cmdButtonState
 {
-    INPUT_CHAR =      1 << 0,
-    INPUT_BACK =      1 << 1,
-    INPUT_ENTER =     1 << 2,
-    INPUT_ESCAPE =    1 << 3
+    INPUT_CHAR =      1u << 0u,
+    INPUT_BACK =      1u << 1u,
+    INPUT_ENTER =     1u << 2u,
+    INPUT_ESCAPE =    1u << 3u
 } CmdButtonState;
 
 
